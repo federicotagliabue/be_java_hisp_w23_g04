5e308348ae6dd219a3962d3509ba59a9e668e3a9
Federico Tagliabue <federico.tagliabue@mercadolibre.com>
2023-12-15 14:53:22 -0300

    add @repository to SocialMediaRepositoryImpl

--- src/main/java/com/sprint/be_java_hisp_w23_g04/repository/SocialMediaRepositoryImpl.java
+++ src/main/java/com/sprint/be_java_hisp_w23_g04/repository/SocialMediaRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.sprint.be_java_hisp_w23_g04.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class SocialMediaRepositoryImpl implements ISocialMediaRepository {
 
 }
