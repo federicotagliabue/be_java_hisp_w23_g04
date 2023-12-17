2f0a877afdb4dfb4b6c9bdc6d3f480d0c1c4db57
fiermarin <matiasfiermarin@gmail.com>
2023-12-17 17:43:55 -0300

    Add feature to calculate follower count for a specific seller

--- src/main/java/com/sprint/be_java_hisp_w23_g04/service/ISocialMediaService.java
+++ src/main/java/com/sprint/be_java_hisp_w23_g04/service/ISocialMediaService.java
@@ -1,9 +1,11 @@
 package com.sprint.be_java_hisp_w23_g04.service;
 
+import com.sprint.be_java_hisp_w23_g04.dto.response.FollowersCountDTO;
 import com.sprint.be_java_hisp_w23_g04.dto.response.UserDTO;
 
 import java.util.List;
 
 public interface ISocialMediaService {
     List<UserDTO> getAllUsers();
+    FollowersCountDTO followersCount(Integer userId);
 }
