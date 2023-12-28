532503993abe2cb31978c8e7561c8545e2dcedf9
camilamamanill <camila.mamanillanquichoque@mercadolibre.com>
2023-12-28 17:30:29 -0300

    add unit tests us-0001

--- src/test/java/com/sprint/be_java_hisp_w23_g04/service/UserMediaServiceImplTest.java
+++ src/test/java/com/sprint/be_java_hisp_w23_g04/service/UserMediaServiceImplTest.java
@@ -1,10 +1,21 @@
 package com.sprint.be_java_hisp_w23_g04.service;
 
+import com.sprint.be_java_hisp_w23_g04.dto.response.SimpleMessageDTO;
+import com.sprint.be_java_hisp_w23_g04.entity.User;
+import com.sprint.be_java_hisp_w23_g04.exception.NotFoundException;
 import com.sprint.be_java_hisp_w23_g04.gateway.UserGatewayImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.sprint.be_java_hisp_w23_g04.utils.UtilsTest.getOneUser;
+import static com.sprint.be_java_hisp_w23_g04.utils.UtilsTest.getOneUserSeller;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserMediaServiceImplTest {
@@ -13,4 +24,51 @@ public class UserMediaServiceImplTest {
 
     @InjectMocks
     UserMediaServiceImpl userService;
+
+    @Test
+    @DisplayName("User Follows Seller User successfully.")
+    void followSellerUserWhenUserExistTestOK(){
+        //Arrange
+        Integer userId = 3;
+        Integer sellerId = 2;
+        SimpleMessageDTO expectedMsg = new SimpleMessageDTO("El usuario con id:3 ahora sigue a vendedor con id:2");
+
+        //Act
+        when(userGateway.findUser(userId)).thenReturn(getOneUser());
+        when(userGateway.findUser(sellerId)).thenReturn(getOneUserSeller());
+        SimpleMessageDTO msg = userService.followSellerUser(userId, sellerId);
+
+        //Assert
+        assertEquals(expectedMsg, msg);
+    }
+
+    @Test
+    @DisplayName("User Follows Seller User fails when User does not exist.")
+    void followSellerUserWhenUserDoesNotExistTestNotFound(){
+        //Arrange
+        Integer userId = 99;
+        Integer sellerId = 2;
+
+        //Act
+        when(userGateway.findUser(userId)).thenReturn(null);
+        when(userGateway.findUser(sellerId)).thenReturn(getOneUserSeller());
+
+        //Assert
+        assertThrows(NotFoundException.class, () -> {userService.followSellerUser(userId, sellerId);});
+    }
+
+    @Test
+    @DisplayName("User Follows Seller User fails when Seller does not exist.")
+    void followSellerUserWhenSellerDoesNotExistTestNotFound(){
+        //Arrange
+        Integer userId = 3;
+        Integer sellerId = 99;
+
+        //Act
+        when(userGateway.findUser(userId)).thenReturn(getOneUser());
+        when(userGateway.findUser(sellerId)).thenReturn(null);
+
+        //Assert
+        assertThrows(NotFoundException.class, () -> {userService.followSellerUser(userId, sellerId);});
+    }
 }
--- src/test/java/com/sprint/be_java_hisp_w23_g04/utils/UtilsTest.java
+++ src/test/java/com/sprint/be_java_hisp_w23_g04/utils/UtilsTest.java
@@ -1,13 +1,30 @@
 package com.sprint.be_java_hisp_w23_g04.utils;
 
 import com.sprint.be_java_hisp_w23_g04.dto.response.BuyerDTO;
+import com.sprint.be_java_hisp_w23_g04.dto.response.SellerDTO;
 import com.sprint.be_java_hisp_w23_g04.dto.response.UserDTO;
+import com.sprint.be_java_hisp_w23_g04.entity.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UtilsTest {
 
     public static BuyerDTO getBuyerDTO() {
         return new BuyerDTO(1, "Juan Perez", List.of(new UserDTO(6, "Diego Lopez"), new UserDTO(4, "Sofia Gomez")));
+    }
+
+    public static User getOneUser() {
+        List <Integer> followedIds = new ArrayList<>();
+        followedIds.add(7);
+        return new User(6, "Diego Lopez", new ArrayList<>(), followedIds, new ArrayList<>());
+    }
+    public static User getOneUserSeller() {
+        List <Integer> followerIds = new ArrayList<>();
+        followerIds.add(2);
+        followerIds.add(4);
+        List <Integer> postIds = new ArrayList<>();
+        postIds.add(4);
+        return new User(3, "Pablo Gonzalez", postIds, new ArrayList<>(), followerIds);
     }
 }
