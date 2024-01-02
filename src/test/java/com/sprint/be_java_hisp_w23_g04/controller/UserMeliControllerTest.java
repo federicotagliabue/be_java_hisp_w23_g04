585c56e0b1f042fca066e3ea378e8dbf62772d6c
camilamamanill <camila.mamanillanquichoque@mercadolibre.com>
2024-01-02 11:29:02 -0300

    add controller test

--- src/test/java/com/sprint/be_java_hisp_w23_g04/controller/UserMeliControllerTest.java
+++ src/test/java/com/sprint/be_java_hisp_w23_g04/controller/UserMeliControllerTest.java
@@ -1,10 +1,15 @@
 package com.sprint.be_java_hisp_w23_g04.controller;
 
 import com.sprint.be_java_hisp_w23_g04.service.UserMediaServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class UserMeliControllerTest {
@@ -13,5 +18,18 @@ public class UserMeliControllerTest {
     UserMediaServiceImpl userService;
 
     @InjectMocks
-    ProductsMeliController controller;
+    UserMeliController controller;
+
+    @Test
+    void followSellerUser() {
+        // arrange
+        int userId = 1;
+        int sellerId = 2;
+
+        // act
+        controller.followSellerUser(userId, sellerId);
+
+        // assert
+        verify(userService, atLeastOnce()).followSellerUser(any(), any());
+    }
 }
