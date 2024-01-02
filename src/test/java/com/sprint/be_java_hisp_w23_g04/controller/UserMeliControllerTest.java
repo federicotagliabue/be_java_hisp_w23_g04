864ad14d2899ed24dfa47e43cdf5eb3707f2e05a
Gabriel El Capo <gabriel.gutierrezsemidey@mercadolibre.com>
2024-01-02 11:19:40 -0300

    Add unit test related to US-0007 (T-0002)
    Add unit test for classes UserMeliController and UserMediaServiceImpl

--- src/test/java/com/sprint/be_java_hisp_w23_g04/controller/UserMeliControllerTest.java
+++ src/test/java/com/sprint/be_java_hisp_w23_g04/controller/UserMeliControllerTest.java
@@ -1,10 +1,28 @@
 package com.sprint.be_java_hisp_w23_g04.controller;
 
+import com.sprint.be_java_hisp_w23_g04.dto.response.SimpleMessageDTO;
+import com.sprint.be_java_hisp_w23_g04.entity.User;
+import com.sprint.be_java_hisp_w23_g04.exception.BadRequestException;
+import com.sprint.be_java_hisp_w23_g04.exception.NotFoundException;
+import com.sprint.be_java_hisp_w23_g04.gateway.UserGatewayImpl;
 import com.sprint.be_java_hisp_w23_g04.service.UserMediaServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserMeliControllerTest {
@@ -13,5 +31,36 @@ public class UserMeliControllerTest {
     UserMediaServiceImpl userService;
 
     @InjectMocks
-    ProductsMeliController controller;
+    UserMeliController controller;
+
+    @Test
+    public void unfollowSuccessfulTest(){
+        int userId = 4;
+        int unfollowedId = 1;
+
+        SimpleMessageDTO expectedServiceResponse = new SimpleMessageDTO("El usuario Juan Perez Id: 1 ya no es seguido por el usuario Sofia Gomez Id: 4");
+        when(userService.unfollowUser(anyInt(), anyInt()))
+                .thenReturn(expectedServiceResponse);
+        ResponseEntity<?> actualResponse = controller.unfollowUser(userId, unfollowedId);
+
+        assertThat(new ResponseEntity<>(expectedServiceResponse,HttpStatus.ACCEPTED)).isEqualTo(actualResponse);
+    }
+
+    @Test
+    public void unfollowUserNotFoundTest(){
+        int userId = 99;
+        int unfollowedId = 2;
+        when(userService.unfollowUser(anyInt(), anyInt()))
+                .thenThrow(new NotFoundException("No se encontró usuario c   on el id " + userId + "."));
+        assertThrows(NotFoundException.class ,()-> controller.unfollowUser(userId, unfollowedId));
+    }
+
+    @Test
+    public void unfollowUserNotFollowedTest(){
+        int userId = 99;
+        int unfollowedId = 2;
+        when(userService.unfollowUser(anyInt(), anyInt()))
+                .thenThrow(new BadRequestException("El vendedor con id:" + userId + " no es seguido por el usuario con id:" + unfollowedId));
+        assertThrows(BadRequestException.class ,()-> controller.unfollowUser(userId, unfollowedId));
+    }
 }
--- src/test/java/com/sprint/be_java_hisp_w23_g04/service/UserMediaServiceImplTest.java
+++ src/test/java/com/sprint/be_java_hisp_w23_g04/service/UserMediaServiceImplTest.java
@@ -1,10 +1,23 @@
 package com.sprint.be_java_hisp_w23_g04.service;
 
+import com.sprint.be_java_hisp_w23_g04.dto.response.SimpleMessageDTO;
+import com.sprint.be_java_hisp_w23_g04.entity.User;
+import com.sprint.be_java_hisp_w23_g04.exception.BadRequestException;
+import com.sprint.be_java_hisp_w23_g04.exception.NotFoundException;
 import com.sprint.be_java_hisp_w23_g04.gateway.UserGatewayImpl;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+import static com.sprint.be_java_hisp_w23_g04.utils.Verifications.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserMediaServiceImplTest {
@@ -13,4 +26,41 @@ public class UserMediaServiceImplTest {
 
     @InjectMocks
     UserMediaServiceImpl userService;
+
+    @Test
+    public void unfollowUserTest(){
+        int userId = 4;
+        int unfollowId = 1;
+
+        when(userGateway.findUser(userId)).thenReturn(new User(4,"Sofia Gomez", List.of(), List.of(1,3),List.of()));
+        when(userGateway.findUser(unfollowId)).thenReturn(new User(1, "Juan Perez", List.of(1,2,7), List.of(), List.of(4,6)));
+
+        SimpleMessageDTO expectedResponse = new SimpleMessageDTO("El usuario Juan Perez Id: 1 ya no es seguido por el usuario Sofia Gomez Id: 4");
+
+        SimpleMessageDTO actualResponse = userService.unfollowUser(userId, unfollowId);
+
+        assertThat(expectedResponse).isEqualTo(actualResponse);
+    }
+
+    @Test
+    public void unfollowUserWithUserNotFoundTest(){
+        int userId = 400;
+        int unfollowId = 1;
+
+        when(userGateway.findUser(userId)).thenReturn(null);
+        when(userGateway.findUser(unfollowId)).thenReturn(new User(1, "Juan Perez", List.of(1,2,7), List.of(), List.of(4,6)));
+
+        assertThrows(NotFoundException.class, ()->userService.unfollowUser(userId, unfollowId), "No se encontró usuario con el id 400.");
+    }
+
+    @Test
+    public void unfollowUserBuyerDontFollowSellerTest(){
+        int userId = 6;
+        int unfollowId = 1;
+
+        when(userGateway.findUser(userId)).thenReturn(new User(6, "Diego Lopez", List.of(), List.of(7), List.of()));
+        when(userGateway.findUser(unfollowId)).thenReturn(new User(1, "Juan Perez", List.of(1,2,7), List.of(), List.of(4,6)));
+
+        assertThrows(BadRequestException.class, ()->userService.unfollowUser(userId, unfollowId), "El usuario con id:6 no sigue al vendedor con id:1");
+    }
 }
